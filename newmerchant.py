43b97561e522cdee238eb58efb2eba93b869461e
urmeo <eti7075@rit.edu>
2020-09-05 12:30:32 -0400

    in lab pre commented

--- newmerchant.py
+++ newmerchant.py
@@ -19,23 +19,31 @@ def read_merchant(filename: str) -> [merchant]:
             ))
     return merches
 
-"""
-def partition(data: list[str], pivot: int) -> Tuple(list[str], list[str], list[str]):
+def partition(data: [merchant], pivot: int) -> ([merchant], [merchant], [merchant]):
     less, equal, greater = [], [], []
     for element in data:
-        if element < pivot:
+        if int(element.loc) < pivot:
             less.append(element)
-        elif element > pivot:
+        elif int(element.loc) > pivot:
             greater.append(element)
         else:
             equal.append(element)
     return less, equal, greater
-"""
+
+def quick_sort(data: [merchant]) -> [merchant]:
+    if len(data) == 0:
+        return []
+    else:
+        pivot = int(data[0].loc)
+        less, equal, greater = partition(data, pivot)
+        return quick_sort(less) + equal + quick_sort(greater)
 
 def main(filename: str):
     Mlst = read_merchant(filename)
-    for merchant in Mlst:
+    sorted_merchants = quick_sort(Mlst)
+    for merchant in sorted_merchants:
         print(merchant.merch_name + " " + merchant.loc)
+    print(sorted_merchants[len(Mlst)//2].merch_name + " " + sorted_merchants[len(Mlst)//2].loc)
 
 
 if __name__ == '__main__':
